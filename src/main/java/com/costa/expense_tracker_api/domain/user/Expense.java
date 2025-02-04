11f3555ac5bf84149ccf1d8b86ac82310f68c62c
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2025-02-04 11:15:10 -0300

    feat: creating entities

---
+++ src/main/java/com/costa/expense_tracker_api/domain/user/Expense.java
@@ -0,0 +1,31 @@
+package com.costa.expense_tracker_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "expense")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Expense {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private Float value;
+    private Date date;
+    private String category;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/com/costa/expense_tracker_api/domain/user/User.java
@@ -0,0 +1,28 @@
+package com.costa.expense_tracker_api.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "user")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private String login;
+    private String password;
+}
