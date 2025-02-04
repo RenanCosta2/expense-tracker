71501cb7d0f8bbcf32629134f612b66627def7ea
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2025-02-04 11:29:36 -0300

    feat: add expense repository

--- src/main/java/com/costa/expense_tracker_api/repositories/ExpenseRepository.java
+++ src/main/java/com/costa/expense_tracker_api/repositories/ExpenseRepository.java
@@ -1,6 +1,6 @@
 package com.costa.expense_tracker_api.repositories;
 
-import com.costa.expense_tracker_api.domain.user.Expense;
+import com.costa.expense_tracker_api.domain.expense.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
