f5a36bf3fe4de5e7f2f08d33c4fb464bbb0d8c72
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2025-02-04 18:30:04 -0300

    feat: query method to find an user by login

--- src/main/java/com/costa/expense_tracker_api/repositories/UserRepository.java
+++ src/main/java/com/costa/expense_tracker_api/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package com.costa.expense_tracker_api.repositories;
 
 import com.costa.expense_tracker_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByLogin(String login);
 }
