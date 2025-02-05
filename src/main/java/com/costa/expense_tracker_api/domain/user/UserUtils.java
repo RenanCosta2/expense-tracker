1dfd920fbd861a110c806a52e4b58fe9f1b844a9
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2025-02-04 23:35:22 -0300

    feat: Utils class with method getUserLogged

---
+++ src/main/java/com/costa/expense_tracker_api/domain/user/UserUtils.java
@@ -0,0 +1,19 @@
+package com.costa.expense_tracker_api.domain.user;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public class UserUtils {
+
+    public static UserDetails getUserLogged(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication != null && authentication.getPrincipal() instanceof UserDetails){
+            return (UserDetails) authentication.getPrincipal();
+        }
+        return null;
+    }
+}
