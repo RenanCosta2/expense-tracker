ca44c50d7c78ee9d8d9f0f4ba9834c809d58b9f6
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2025-02-10 14:30:20 -0300

    refactor: move user login logic from controller to UserService

--- src/main/java/com/costa/expense_tracker_api/controllers/AuthenticationController.java
+++ src/main/java/com/costa/expense_tracker_api/controllers/AuthenticationController.java
@@ -1,17 +1,10 @@
 package com.costa.expense_tracker_api.controllers;
 
 import com.costa.expense_tracker_api.domain.user.*;
-import com.costa.expense_tracker_api.exceptions.InvalidCredentialsException;
-import com.costa.expense_tracker_api.exceptions.UserAlreadyExistsException;
-import com.costa.expense_tracker_api.infra.security.TokenService;
-import com.costa.expense_tracker_api.repositories.UserRepository;
+import com.costa.expense_tracker_api.services.AuthorizationService;
 import com.costa.expense_tracker_api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,28 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private TokenService tokenService;
-
-    @Autowired
     private UserService userService;
+
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthenticationDTO data){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
 
-        try{
-            var auth = this.authenticationManager.authenticate(usernamePassword);
+        LoginResponseDTO token = userService.loginUser(data);
 
-            var token = tokenService.generateToken((User) auth.getPrincipal());
-
-            return ResponseEntity.ok(new LoginResponseDTO(token));
-
-        } catch (InternalAuthenticationServiceException exception) {
-            throw new InvalidCredentialsException();
-        }
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/register")
--- src/main/java/com/costa/expense_tracker_api/services/UserService.java
+++ src/main/java/com/costa/expense_tracker_api/services/UserService.java
@@ -1,11 +1,14 @@
 package com.costa.expense_tracker_api.services;
 
-import com.costa.expense_tracker_api.domain.user.RegisterDTO;
-import com.costa.expense_tracker_api.domain.user.User;
-import com.costa.expense_tracker_api.domain.user.UserResponseDTO;
+import com.costa.expense_tracker_api.domain.user.*;
+import com.costa.expense_tracker_api.exceptions.InvalidCredentialsException;
 import com.costa.expense_tracker_api.exceptions.UserAlreadyExistsException;
+import com.costa.expense_tracker_api.infra.security.TokenService;
 import com.costa.expense_tracker_api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,12 @@ public class UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
 
     public UserResponseDTO createUser(RegisterDTO data){
 
@@ -33,6 +42,21 @@ public class UserService {
 
         return new UserResponseDTO(newUser.getId(), newUser.getName(), newUser.getLogin(), newUser.getRole().getRole());
 
+    }
+
+    public LoginResponseDTO loginUser(AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+
+        try{
+            var auth = this.authenticationManager.authenticate(usernamePassword);
+
+            var token = tokenService.generateToken((User) auth.getPrincipal());
+
+            return new LoginResponseDTO(token);
+
+        } catch (InternalAuthenticationServiceException exception) {
+            throw new InvalidCredentialsException();
+        }
     }
 
 }
